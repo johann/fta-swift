de14c1ae6e1d58983e9d05b993f20a10d823e720
Johann Kerr <johannkerr@gmail.com>
2017-02-11 20:03:27 -0500

    fixed vapor

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "fta",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 4),
         .Package(url: "https://github.com/nodes-vapor/storage", majorVersion: 0),
         .Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 0)
     ],
